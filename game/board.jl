88741540c9525a99ac86c6b98029d6de43a18e4e
Ratan <ratan.r.sur@gmail.com>
2015-09-24 13:21:01 -0400

    move drawing funcs

--- game/board.jl
+++ game/board.jl
@@ -108,32 +108,3 @@ function move_player!(board::Board, direction::String)
         move!(board.player, direction)
         update_player_pos(board)
 end
-
-function print_cell(board::Board, y, x)
-        board.matrix_representations[board.active,y,x,1] == 1 && (print('@'); return)
-        board.matrix_representations[board.active,y,x,2] == 1 && (print('+'); return)
-        board.matrix_representations[board.active,y,x,3] == 1 && (print('*'); return)
-        print(' ')
-end
-
-function draw(board::Board)
-        print("+")
-        for i=1:board.width
-                print("-")
-        end
-        print("+")
-        print('\n')
-        for y = 1:board.height
-                print('|')
-                for x = 1:board.width
-                        print_cell(board,y,x)
-                end
-                print('|')
-                print('\n')
-        end
-        print("+")
-        for i=1:board.width
-                print("-")
-        end
-        print("+\n")
-end
